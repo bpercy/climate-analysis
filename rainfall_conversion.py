6ca32859df255462c475529c799ed469e83de723
fkhunt <freja.hunt@the-iea.org>
2017-08-02 15:27:50 +0100

    New file for rainfall unit conversion

---
+++ rainfall_conversion.py
@@ -0,0 +1,11 @@
+""" A library to perform rainfall unit conversions"""
+
+def inces_to_mm(inches):
+    """Convert inces to mm
+	
+	Arguements:
+	inches -- the rainfall in inches
+	"""
+	
+	mm = inches * 25.4
+	return mm
